42a30941a66e21a08ea96e14f311f7fdde66cc51
Gastaccount CCS <cernadavid1@gmail.com!>
2022-02-22 12:24:59 +0100

    Made the updates

--- sample.py
+++ sample.py
@@ -2,76 +2,131 @@ import fire #type: ignore
 from dilp import *
 import torch
 from tqdm import tqdm #type: ignore
+import pyparsing as pp
+import  torch
+from core import Term, Atom
 
-def main(epochs : int = 100, steps : int = 5, cuda : bool = False):
+# relationship will refer to 'track' in all of your examples
+relationship = pp.Word(pp.alphas).setResultsName('relationship', listAllMatches=True)
+
+number = pp.Word(pp.nums + '.')
+variable = pp.Word(pp.alphas)
+# an argument to a relationship can be either a number or a variable
+argument = number | variable
+
+# arguments are a delimited list of 'argument' surrounded by parenthesis
+arguments = (pp.Suppress('(') + pp.delimitedList(argument) +
+             pp.Suppress(')')).setResultsName('arguments', listAllMatches=True)
+
+# a fact is composed of a relationship and it's arguments
+# (I'm aware it's actually more complicated than this
+# it's just a simplifying assumption)
+fact = (relationship + arguments).setResultsName('facts', listAllMatches=True)
+
+# a sentence is a fact plus a period
+sentence = fact + pp.Suppress('.')
+
+# self explanatory
+prolog_sentences = pp.OneOrMore(sentence)
+
+def revDict(d):
+    return {value: key for (key,value) in d.items()}
+
+def process_file(filename):
+    atoms = []
+    predicates = set()
+    constants = set()
+    with open(filename) as f:
+        data = f.read().replace('\n', '')
+        result = prolog_sentences.parseString(data)
+        for idx in range(len(result['facts'])):
+            fact = result['facts'][idx]
+            predicate = result['relationship'][idx]
+            terms = [Term(False, term) for term in result['arguments'][idx]]
+            term_var = [Term(True, f'X_{i}') for i in range(len(terms))]
+
+            predicates.add(Atom(term_var, predicate))
+            atoms.append(Atom(terms, predicate))
+            constants.update([Term(False, term) for term in result['arguments'][idx]])
+    return atoms, predicates, constants
+
+def main(epochs : int = 100, steps : int = 5, cuda : bool = False, task : str = "", inv : int = 10 ):
     dev = torch.device(0) if cuda else torch.device('cpu')
 
-    #logging.getLogger().setLevel(logging.DEBUG)
-    base_val = torch.as_tensor([
-        [ #false
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-        ], [ #succ
-            [0,1,0,0,0],
-            [0,0,1,0,0],
-            [0,0,0,1,0],
-            [0,0,0,0,1],
-            [0,0,0,0,0]
-        ], [ #p
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-        ], [ #q
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-        ]
-    ], dtype=torch.float, device=dev)
-    
-    rulebook = Rulebook(
-        body_predicates=torch.as_tensor([
-            [[[0,0] for _ in range(0,11)] for _ in range(0,2)],
-            [[[0,0] for _ in range(0,11)] for _ in range(0,2)],
-            [[[1,1],[1,1],[1,1],[1,1],[3,3],[2,0],[1,1],[0,0],[1,2],[2,1],[2,2]] for _ in range(0,2)],
-            [[[1,1],[1,1],[1,1],[1,1],[3,3],[2,0],[1,1],[0,0],[1,2],[2,1],[2,2]] for _ in range(0,2)],
-        ], device=dev),
-        variable_choices=torch.as_tensor([
-            [[[0,0] for _ in range(0,11)] for _ in range(0,2)],
-            [[[0,0] for _ in range(0,11)] for _ in range(0,2)],
-            [[[0,0],[1,0],[2,7],[5,4],[3,3],[2,0],[1,1],[0,0],[1,2],[2,1],[2,2]] for _ in range(0,2)],
-            [[[0,0],[1,0],[2,7],[5,4],[3,3],[2,0],[1,1],[0,0],[1,2],[2,1],[2,2]] for _ in range(0,2)],
-        ], device=dev)
-    )
+    if task == "":
+        raise Exception('A task must be provided')
+    if  inv<0:
+            raise Exception('The number of invented predicates must be >= 0')
+
+
+    B, pred_f, constants_f = process_file('%s/facts.dilp' % task)
+    P, target_p, constants_p = process_file('%s/positive.dilp' % task)
+    N, target_n, constants_n = process_file('%s/negative.dilp' % task)
+
+    if not (target_p == target_n):
+        raise Exception('Positive and Negative files have different targets')
+    elif not len(target_p) == 1:
+        raise Exception('Can learn only one predicate at a time')
+    elif not constants_n.issubset(constants_f) or not constants_p.issubset(constants_f):
+        raise Exception(
+            'Constants not in fact file exists in positive/negative file')
+
+    invented_names= ["inv_"+str(x) for x in range(inv)]
+    target = list(target_p)[0].predicate
+    target_arity = P[0].arity
+    count_examples = len(P)+len(N)
+    pred_dim = len(pred_f)+inv+2
+    atom_dim = len(constants_f)
+    rules_dim = (pred_dim**2)*81
+    fact_names = [ x.predicate for x in pred_f]
+    true_facts = B+P
+    target_facts = P+N
+    var_names = [Term(True, f'X_{i}') for i in range(3)]
+
+    pred_dict = dict(zip(list(range(pred_dim)),[ x.predicate for x in pred_f]+ [target,"false"]+invented_names))
+    atom_dict = dict(zip(list(range(atom_dim)),list(constants_f)))
+    rules_dict = dict(zip(list(range(rules_dim)),[(x,y,z,w) for x in pred_dict.values() \
+    for y in pred_dict.values() for z in range(9) for w in range(9)]))
+    var_dict   = dict(zip(list(range(9)),[ (x,y) for x in var_names for y in var_names]))
+
+    # reverse version of the dictionaries
+    pred_dict_rev,atom_dict_rev,rules_dict_rev,var_dict_rev = revDict(pred_dict),revDict(atom_dict), \
+    revDict(rules_dict),revDict(var_dict)
+
+    base_val = torch.zeros([pred_dim, atom_dim, atom_dim], dtype=torch.float, device=dev)
+    body_predicates = torch.zeros([pred_dim, 2, rules_dim,2], dtype=torch.long, device=dev)
+    variable_choices = torch.zeros([pred_dim, 2, rules_dim,2], dtype=torch.long, device=dev)
+    targets = torch.full([count_examples,target_arity+1], pred_dict_rev[target], dtype=torch.float, device=dev)
+    target_values = torch.zeros([count_examples], dtype=torch.float, device=dev)
+
+    for x in range(pred_dim):
+        for y in range(atom_dim):
+            for z in range(atom_dim):
+                if Atom([atom_dict[y],atom_dict[z]],pred_dict[x]) in true_facts:
+                    base_val[x][y][z] = 1
+
+    for x in range(pred_dim):
+        for y in range(2):
+            for z in range(rules_dim):
+                for w in range(2):
+                    body_predicates[x][y][z][w] = pred_dict_rev["false"] if pred_dict[x] in \
+                    fact_names else pred_dict_rev[rules_dict[z][w]]
+                    variable_choices[x][y][z][w] = rules_dict[z][2+w]
+
+    for x in range(len(target_facts)):
+        for y in range(target_arity):
+            targets[x][y+1] = atom_dict_rev[target_facts[x].terms[y]]
+        if x < len(P):
+            target_values[x] = 1
+
+    print("done")
+    rulebook = Rulebook(body_predicates,variable_choices)
     logging.debug(f"{rulebook.body_predicates.shape=},{rulebook.variable_choices.shape=}")
-    pred_names = ['false', 'succ', 'p', 'q']
 
-    targets = torch.as_tensor([
-        [2,0,2],
-        [2,1,3],
-        [2,2,4],
-        [2,1,1],
-        [2,3,2],
-        [2,0,0],
-        [2,0,1]
-    ], device=dev)
-    target_values = torch.as_tensor([
-        1.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    ], device=dev)
+#This should not be used. Instead one of the dictionaries should be used.
+    pred_names = pred_dict_rev.keys()
 
-    weights = torch.nn.Parameter(torch.rand(size=(4,2,11), device=dev))
+    weights = torch.nn.Parameter(torch.rand(size=(pred_dim,2,rules_dim), device=dev))
     #weights = torch.nn.Parameter(torch.zeros(4,2,11))
     #with torch.no_grad():
     #    weights[2][0][2] = 1000
