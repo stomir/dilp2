8d4db65e8c134c3e9bf75ed767e3d08880b1ea53
Stanislaw Purgal <stanislaw.purgal@uibk.ac.at>
2023-05-07 18:42:59 +0200

    buzz form dilp1 example

---
+++ examples/gen_buzz_as_dilp1_small.py
@@ -0,0 +1,28 @@
+from genutils import *
+from typing import *
+
+class Train:
+    def world1(x) -> Iterable:
+        yield from range(11)
+
+class Validate:
+    def world1(x) -> Iterable:
+        yield from range(21)
+
+class BK:
+    def zero(a,b):
+        return a == 0 and b == 0
+
+    def succ(a,b):
+        return a+1 == b
+
+    def plus2(a,b):
+        return a+2 == b
+
+    def plus3(a,b):
+        return a+3 == b
+
+class Targets:
+    def buzz(a,b):
+        if a != b: raise Skip
+        return a % 5 == 0
